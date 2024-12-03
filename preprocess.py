63e34fb2931f7800b674d92dc414107e5223ff80
Thomas McInnes <tommcinnes.03@gmail.com>
2024-12-02 17:33:40 -0700

    Uncommented lines i meant to uncomment before

--- preprocess.py
+++ preprocess.py
@@ -7,15 +7,15 @@ INCIDENT_COLS = [1, 9, 10, 19, 20]
 OFFENSE_COLS = [2, 3, 4, 5, 6, 7, 8, ]
 LOCATION_COLS = [13, 14, 15, 16, 17, 21, 22]
 
-# with open(input_file, 'r') as input, open(output_file, 'w', newline='') as output:
-#   reader = csv.reader(input)
-#   writer = csv.writer(output)
+with open(input_file, 'r') as input, open(output_file, 'w', newline='') as output:
+  reader = csv.reader(input)
+  writer = csv.writer(output)
 
-#   for row in reader:
-#     while len(row) < N_COLUMNS:
-#       row.append('')
+  for row in reader:
+    while len(row) < N_COLUMNS:
+      row.append('')
 
-#     writer.writerow(row)
+    writer.writerow(row)
 
 with open("CLEANED_Denver_Crime.csv", 'r') as input, open("./incident.csv", 'w', newline='') as incident, open("offense.csv", 'w', newline='') as offense, open("location.csv", 'w', newline='') as location:
   reader = csv.reader(input)
